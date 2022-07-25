70cad45ce5a810915eeb5b1fc8bd9e38ee7e498e
DemonDis <50297797+DemonDis@users.noreply.github.com>
2022-07-25 23:35:17 +0700

    start server express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
- console.log('Hi !');
+const express = require('express')
+const mongoose = require("mongoose")
+const config = require("config")
+
+const app = express()
+const port = config.get('serverPort')
+
+app.get('/', (req, res) => {
+    try {
+        mongoose.connect(config.get("dbUrl"))
+        res.send('Hellow world!!!')
+    } catch(error) {
+
+    }
+})
+
+app.listen(port, () => {
+    console.log('Server started on port ', port)
+})
+
